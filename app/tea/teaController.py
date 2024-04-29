fa8b46e40dfc8d7150ceaf29991dc2e9109e24c2
juhoh <zestina.oh@gmail.com>
2024-04-29 22:51:31 +0900

    update: /tea API
    - according to new endpoint
    - remove try~except
    - use 'with' for cursor
    - update Schema
    - use snake_case
    
    related issue: #1, #14, #19, #12

--- app/tea/teaController.py
+++ app/tea/teaController.py
@@ -1,32 +1,57 @@
 from app.db import conn
 from flask_restx import Namespace, Resource, fields
 from . import teaService as serv
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from ..user.userUtils import update_location
+from ..wrapper.location import update_location
+from ..wrapper.token import custom_jwt_required
 
-ns = Namespace(name='tea', description='추천 관련 API', path='/tea')
-
-class _Schema():
-    response_fields = ns.model('응답 내용', {
-        'message': fields.String(description='응답 별 참고 메시지'),
-        'data': fields.Raw(description='API별 필요한 응답 내용')
-    })
+ns = Namespace(name="tea", description="추천 관련 API", path="/tea")
 
 
-@ns.route('/')
-@ns.header('content-type', 'application/json')
+class _ResponseSchema:
+    field_get_profile = ns.model(
+        "profile 확인 응답 데이터",
+        {
+            "id": fields.Integer(description="유저 id"),
+            "name": fields.String(description="유저 이름"),
+            "last_name": fields.String(description="유저 성"),
+            "distance": fields.Float(description="거리"),
+            "fancy": fields.Integer(description="유저와의 fancy 관계"),
+            "age": fields.Integer(description="유저 나이"),
+            "fame": fields.Float(description="fame 지수"),
+            "tags": fields.List(fields.Integer, description="설정한 관심사 태그"),
+            "gender": fields.Integer(description="유저 성별"),
+            "picture": fields.List(
+                fields.String, description="유저 프로필 사진 데이터"
+            ),
+        },
+    )
+
+    field_get_tea = ns.model(
+        "tea 추천 응답 데이터",
+        {
+            "profiles": fields.List(
+                fields.Nested(field_get_profile), description="profile JSON list"
+            ),
+        },
+    )
+
+    field_failed = ns.model(
+        "API 요청 실패",
+        {
+            "message": fields.String(description="실패 이유"),
+        },
+    )
+
+
+@ns.route("/")
 class Suggest(Resource):
-    # @jwt_required()
-    @ns.response(200, 'api요청 성공', _Schema.response_fields)
-    @ns.response(400, 'api요청 실패', _Schema.response_fields)
-    # @update_location
-    def get(self):
+    # @custom_jwt_required()
+    @ns.response(200, "api요청 성공", _ResponseSchema.field_get_tea)
+    @ns.response(400, "Bad Request", _ResponseSchema.field_failed)
+    @ns.response(403, "Forbidden(권한없음)", _ResponseSchema.field_failed)
+    # @update_location()
+    def get(self, id=1):
         """get tea suggestions for you!"""
-        try:
-            id = 1
-            # id = get_jwt_identity()['id']
-            return serv.suggest(id)
-        except Exception as e:
-            conn.rollback()
-            print(f'BE error: {self} {e}')
-            return { 'message': 'failed' }, 400
+        # [JWT] delete below
+        id = 1
+        return serv.suggest(id)
--- app/tea/teaService.py
+++ app/tea/teaService.py
@@ -1,53 +1,59 @@
 from app.db import conn
-from datetime import datetime, timedelta
-from app.const import MAX_SUGGEST, AGE_GAP, Gender, AREA_DISTANCE, EARTH_RADIUS, DAYS
+from app.const import (
+    MAX_SUGGEST,
+    AGE_GAP,
+    Gender,
+    AREA_DISTANCE,
+    MIN_AGE,
+    StatusCode,
+)
 import app.user.userUtils as userUtils
 import app.history.historyUtils as historyUtils
 from psycopg2.extras import DictCursor
 
 
-
 def suggest(id):
-    
-    #TODO 모두 세팅된 유저만 나오게 처리 (회원가입, 사진, 태그, 이모지)
-    # 자기자신, block user 안가져오기
+
     # 성적 취향이 서로 맞고
     # 싫은게 하나도 없고 (hate_tag, hate_emoji)
     # 겹치는거 많은 순(tag, emoji)
     # 유저와 같은 지역에 fame rating 높은 사람으로
 
-    # cursor = None
-    try:
-        cursor = conn.cursor(cursor_factory=DictCursor)
-        sql = 'SELECT * FROM "User" WHERE "id" = %s;'
-        cursor.execute(sql, (id, ))
+    user = userUtils.get_user(id)
+    if not user:
+        return {
+            "message": "유저 정보를 찾을 수 없습니다.",
+        }, StatusCode.UNAUTHORIZED
 
-        user = cursor.fetchone()
-        if not user:
-            cursor.close()
-            return {
-                'message': 'no such user',
-            }, 400
+    tags, hate_tags = user["tags"], user["hate_tags"]
+    emoji, hate_emoji = user["emoji"], user["hate_emoji"]
+    long, lat = user["longitude"], user["latitude"]
+    similar = user["similar"]
 
-        tags, hate_tags = user['tags'], user['hate_tags']
-        emoji, hate_emoji = user['emoji'], user['hate_emoji']
-        long, lat = user['longitude'], user['latitude']
-        similar = user['similar']
+    if MIN_AGE <= user["age"]:
+        min_age = max(user["age"] - AGE_GAP, MIN_AGE)
+        max_age = user["age"] + AGE_GAP
+    else:
+        min_age = max_age = MIN_AGE
 
-        # 나이 차이 범위 AGE_GAP
-        date_start = user['birthday'] - timedelta(AGE_GAP * DAYS)
-        date_end = user['birthday'] + timedelta(AGE_GAP * DAYS)
+    find_taste = user["gender"] | Gender.OTHER
+    find_gender = Gender.ALL if user["taste"] & Gender.OTHER else user["taste"]
 
-        find_taste = user['gender'] | Gender.OTHER
-        find_gender = Gender.ALL if user['taste'] & Gender.OTHER else user['taste']
-
-        sql = 'SELECT * \
-                FROM "User" \
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+        sql = 'SELECT * FROM ( \
+                            SELECT *, \
+                                ST_Distance( \
+                                    ST_MakePoint("longitude", "latitude"), \
+                                    ST_MakePoint(%s, %s) \
+                                ) AS distance \
+                            FROM "User" \
+                        ) AS user_distance \
                 WHERE "id" != %s \
-                    AND "User"."id" NOT IN ( \
+                    AND "id" NOT IN ( \
                             SELECT "target_id" \
                             FROM "Block" \
                             WHERE "user_id" = %s ) \
+                    AND "emoji" IS NOT NULL \
                     AND "taste" & %s > 0 \
                     AND "gender" & %s > 0 \
                     AND "hate_tags" & %s = 0 \
@@ -58,49 +64,42 @@ def suggest(id):
                     AND CASE WHEN %s THEN "emoji" & %s > 0 \
                             ELSE "emoji" & %s = 0 \
                         END \
-                    AND "birthday" BETWEEN %s AND %s \
-                    AND ( %s * acos( \
-                                cos(radians("latitude")) * cos(radians(%s)) * \
-                                cos(radians("longitude" - %s)) + \
-                                sin(radians("latitude")) * sin(radians(%s)) \
-                            ) \
-                        ) < %s \
+                    AND "age" BETWEEN %s AND %s \
+                    AND "distance" <= %s \
                 ORDER BY CASE WHEN %s THEN "emoji" & %s \
                         END DESC, \
-                        "count_fancy" / COALESCE("count_view", 1) DESC \
+                        distance ASC, \
+                        "count_fancy"::float / COALESCE("count_view", 1) DESC \
                 LIMIT %s ;'
-        
-        cursor.execute(sql, (id, id,
-                            find_taste, find_gender,
-                            tags, hate_tags,
-                            emoji, hate_emoji,
-                            tags, similar, emoji, emoji,
-                            date_start, date_end,
-                            EARTH_RADIUS, lat, long, lat, AREA_DISTANCE,
-                            similar, emoji, MAX_SUGGEST))
+
+        cursor.execute(
+            sql,
+            (
+                long,
+                lat,
+                id,
+                id,
+                find_taste,
+                find_gender,
+                tags,
+                hate_tags,
+                emoji,
+                hate_emoji,
+                tags,
+                similar,
+                emoji,
+                emoji,
+                min_age,
+                max_age,
+                AREA_DISTANCE,
+                similar,
+                emoji,
+                MAX_SUGGEST,
+            ),
+        )
         db_data = cursor.fetchall()
 
-        result = []
-        for target in db_data:
-
-            result.append({
-                'id': target['id'],
-                'name': target['name'],
-                'last_name': target['last_name'],
-                'birthday': datetime.strftime(target['birthday'], '%Y-%m-%d'),
-                'distance': userUtils.get_distance(lat, long, target['latitude'], target['longitude']),
-                'fame': target['count_fancy'] / target['count_view'] * 10 if target['count_view'] else 0,
-                'tags': userUtils.decodeBit(target['tags']),
-                'fancy': historyUtils.getFancy(id, target['id']),
-            })
-
-        cursor.close()
+        result = [userUtils.get_profile(id, target["id"]) for target in db_data]
         return {
-            'message': 'succeed',
-            'data': result,
-        }, 200
-    
-    except Exception as e:
-        if cursor:
-            cursor.close()
-        raise e
+            "profiles": result,
+        }, StatusCode.OK
