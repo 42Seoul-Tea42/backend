1a5984821c56eec1e3a94b38c44fee9bd671423e
juhoh <zestina.oh@gmail.com>
2024-06-12 22:08:09 +0900

    fix [Socket] handler - move handler to wsgi.py - remove event.py
    related issue: #9

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,11 @@ gevent.monkey.patch_all()
 
 from app import create_app
 import socketio
+from app.socket import socketService as socketServ
+from gevent.lock import Semaphore
+import sys
+
+socket_lock = Semaphore(1)
 
 socket_io = socketio.Server(
     async_mode="gevent",
@@ -12,9 +17,42 @@ socket_io = socketio.Server(
     logger=True,
 )
 
-
 application = create_app()
 application = socketio.WSGIApp(socket_io, application)
+
+
+#### connect && disconnect ####
+@socket_io.on("connect")
+def handle_connect(sid, environ, auth=None):
+    from app.user.userUtils import validation
+
+    if not auth or not auth["id"] or not validation(auth["id"], int):
+        print("auth error: ", auth)
+        sys.stdout.flush()
+        return False
+
+    socketServ.handle_connect(int(auth["id"]), sid)
+
+
+@socket_io.on("disconnect")
+def handle_disconnect(sid):
+    socketServ.handle_disconnect(sid)
+
+
+# #### chat ####
+@socket_io.on("send_message")
+def send_message(sid, data):
+    socketServ.send_message(data, sid)
+
+
+@socket_io.on("read_message")
+def read_message(sid, data):
+    socketServ.read_message(data, sid)
+
+
 if __name__ == "__main__":
+    # # [TEST]
+    # application = create_app()
+    # application = socketio.WSGIApp(socket_io, application)
 
     socket_io.run(application, debug=application.config["DEBUG"])
