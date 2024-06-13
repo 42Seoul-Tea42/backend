1874319056065b6b7539754e9ce27641505535e9
juhoh <zestina.oh@gmail.com>
2024-06-13 22:52:17 +0900

    update [Socket] use socket_lock on handler - remove try-except from socketServ
    related issue: #9

--- app/socket/socketService.py
+++ app/socket/socketService.py
@@ -15,294 +15,196 @@ id_match = dict()
 def handle_connect(id, user_sid):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
-        redis_user = redisServ.get_user_info(id, RedisOpt.LOGIN)
-        if redis_user is None:
-            with socket_lock:
-                socket_io.emit(
-                    "conn_fail", {"message": "존재하지 않는 유저입니다."}, room=user_sid
-                )
+    redis_user = redisServ.get_user_info(id, RedisOpt.LOGIN)
+    if redis_user is None:
+        socket_io.emit(
+            "conn_fail", {"message": "존재하지 않는 유저입니다."}, room=user_sid
+        )
 
-        error_flag += 1
-        # (redis) user_info 업데이트
-        redisServ.update_user_info(id, {"socket_id": user_sid})
+    # (redis) user_info 업데이트
+    redisServ.update_user_info(id, {"socket_id": user_sid})
 
-        error_flag += 1
-        # (redis) socket_id 저장
-        with socket_lock:
-            socket_io.save_session(user_sid, {"id": id})
+    # (redis) socket_id 저장
+    socket_io.save_session(user_sid, {"id": id})
 
-        error_flag += 1
-        # (socket) status 업데이트
-        id_match[id] = historyUtils.get_match_list(id)
-        _update_status(socket_io, socket_lock, id, UserStatus.ONLINE, id_match)
-
-    except Exception as e:
-        print("handle_connect" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    # (socket) status 업데이트
+    id_match[id] = historyUtils.get_match_list(id)
+    _update_status(socket_io, id, UserStatus.ONLINE, id_match)
 
 
 def handle_disconnect(user_sid):
-    from wsgi import socket_io, socket_lock
+    from wsgi import socket_io
 
-    error_flag = 1
-    try:
-        # (socket) status 업데이트
-        id = None
-        with socket_lock:
-            id = socket_io.get_session(user_sid).get("id", None)
-            if id is None:
-                return
+    # (socket) status 업데이트
+    id = socket_io.get_session(user_sid).get("id", None)
+    if id is None:
+        return
 
-        error_flag += 1
-        _update_status(socket_io, socket_lock, id, UserStatus.OFFLINE, id_match)
+    _update_status(socket_io, id, UserStatus.OFFLINE, id_match)
 
-        error_flag += 1
-        # match 정보 삭제 및 (redis) 데이터 삭제
-        if id in id_match:
-            id_match.pop(id)
-        redisServ.delete_socket_id_by_id(id)
+    # match 정보 삭제 및 (redis) 데이터 삭제
+    if id in id_match:
+        id_match.pop(id)
+    redisServ.delete_socket_id_by_id(id)
 
-        error_flag += 1
-        userUtils.update_last_online(id)
-
-    except Exception as e:
-        print("handle_disconnect" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    userUtils.update_last_online(id)
 
 
-def _update_status(socket_io, socket_lock, id, status, id_match):
-
+def _update_status(socket_io, id, status, id_match):
     # status 업데이트
     for target_id in id_match.get(id, []):
         target_sid = redisServ.get_socket_id_by_id(target_id)
         if target_sid:
-            with socket_lock:
-                socket_io.emit(
-                    "update_status",
-                    {"target_id": id, "status": status},
-                    room=target_sid,
-                )
+            socket_io.emit(
+                "update_status",
+                {"target_id": id, "status": status},
+                room=target_sid,
+            )
 
 
 ### chat ###
 def send_message(data, user_sid=None):
-    from wsgi import socket_io, socket_lock
+    from wsgi import socket_io
 
-    error_flag = 1
-    try:
-        # [Test]
-        sender_id = None
-        with socket_lock:
-            sender_id = socket_io.get_session(user_sid).get("id", None)
-            if sender_id is None:
-                return
+    # [Test]
+    sender_id = socket_io.get_session(user_sid).get("id", None)
+    if sender_id is None:
+        return
 
-        error_flag += 1
-        # TODO type 검사
-        recver_id = data.get("recver_id")  # int
-        message = data.get("message")  # str
+    # TODO type 검사
+    recver_id = data.get("recver_id")  # int
+    message = data.get("message")  # str
 
-        error_flag += 1
-        if recver_id and message.strip():
-            msg_time = chatUtils.save_chat(sender_id, recver_id, message)
+    if recver_id not in id_match.get(sender_id, set()):
+        return
 
-            error_flag += 1
-            recver_sid = redisServ.get_socket_id_by_id(recver_id)
-
-            error_flag += 1
-            if recver_sid:
-
-                error_flag += 1
-                with socket_lock:
-                    socket_io.emit(
-                        "send_message",
-                        {
-                            "sender_id": sender_id,
-                            "message": message,
-                            "msg_time": msg_time,
-                            "msg_new": True,
-                        },
-                        room=recver_sid,
-                    )
-        else:  # [TEST]
-            print("send_message ERRRRRRRRRRRR")
-            sys.stdout.flush()
-
-    except Exception as e:
-        print("send_message" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    if recver_id and message.strip():
+        msg_time = chatUtils.save_chat(sender_id, recver_id, message)
+        recver_sid = redisServ.get_socket_id_by_id(recver_id)
+        if recver_sid:
+            socket_io.emit(
+                "send_message",
+                {
+                    "sender_id": sender_id,
+                    "message": message,
+                    "msg_time": msg_time,
+                    "msg_new": True,
+                },
+                room=recver_sid,
+            )
 
 
 def read_message(data, user_sid):
-    from wsgi import socket_io, socket_lock
+    from wsgi import socket_io
 
-    error_flag = 1
-    try:
-        recver_id = None
-        with socket_lock:
-            recver_id = socket_io.get_session(user_sid).get("id", None)
-            if recver_id is None:
-                return
+    recver_id = socket_io.get_session(user_sid).get("id", None)
+    if recver_id is None:
+        return
 
-        error_flag += 1
-        # TODO type 검사
-        sender_id = data.get("sender_id")  # int
-        sender_sid = redisServ.get_socket_id_by_id(sender_id)
+    # TODO type 검사
+    sender_id = data.get("sender_id")  # int
+    sender_sid = redisServ.get_socket_id_by_id(sender_id)
 
-        error_flag += 1
-        if sender_sid:
-            with socket_lock:
-                socket_io.emit(
-                    "read_message", {"recver_id": recver_id}, room=sender_sid
-                )
+    if sender_sid:
+        socket_io.emit("read_message", {"recver_id": recver_id}, room=sender_sid)
 
-        error_flag += 1
-        # (DB) message read 처리
-        chatUtils.read_chat(recver_id, sender_id)
-
-    except Exception as e:
-        print("read_message" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    # (DB) message read 처리
+    chatUtils.read_chat(recver_id, sender_id)
 
 
 #### alarm ####
 def new_match(id, target_id):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
+    user_sid = redisServ.get_socket_id_by_id(id)
+    target_sid = redisServ.get_socket_id_by_id(target_id)
 
-        user_sid = redisServ.get_socket_id_by_id(id)
-        target_sid = redisServ.get_socket_id_by_id(target_id)
+    # MongoDB에 신규 대화 생성
+    chatUtils.save_chat(id, target_id, "")
 
-        error_flag += 1
-        # MongoDB에 신규 대화 생성
-        chatUtils.save_chat(id, target_id, "")
+    # socket alarm 발생
+    if user_sid:
+        id_match.get(id, set()).add(target_id)
+        with socket_lock:
+            socket_io.emit("new_match", {"target_id": target_id}, room=user_sid)
 
-        error_flag += 1
-        # socket alarm 발생
-        if user_sid:
-            error_flag += 1
-            id_match.get(id, set()).add(target_id)
-            error_flag += 1
-            with socket_lock:
-                socket_io.emit("new_match", {"target_id": target_id}, room=user_sid)
-        if target_sid:
-            error_flag += 3
-            id_match.get(target_id, set()).add(id)
-            error_flag += 1
-            with socket_lock:
-                socket_io.emit("new_match", {"target_id": id}, room=target_sid)
-
-    except Exception as e:
-        print("new_match" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    if target_sid:
+        id_match.get(target_id, set()).add(id)
+        with socket_lock:
+            socket_io.emit("new_match", {"target_id": id}, room=target_sid)
 
 
 def new_fancy(id, target_id):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
-        target_sid = redisServ.get_socket_id_by_id(target_id)
-        if target_sid:
-            with socket_lock:
-                socket_io.emit("new_fancy", {"target_id": id}, room=target_sid)
-
-    except Exception as e:
-        print("new_fancy" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    target_sid = redisServ.get_socket_id_by_id(target_id)
+    if target_sid:
+        with socket_lock:
+            socket_io.emit("new_fancy", {"target_id": id}, room=target_sid)
 
 
 def new_history(id):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
-        user_sid = redisServ.get_socket_id_by_id(id)
-        if user_sid:
-            with socket_lock:
-                socket_io.emit("new_history", room=user_sid)
-
-    except Exception as e:
-        print("new_history" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    user_sid = redisServ.get_socket_id_by_id(id)
+    if user_sid:
+        with socket_lock:
+            socket_io.emit("new_history", room=user_sid)
 
 
 #### update ####
 def update_distance(id, lat, long):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
-        for target_id in id_match.get(id, set()):
+    for target_id in id_match.get(id, set()):
 
-            error_flag += 1
-            target_sid = redisServ.get_socket_id_by_id(target_id)
+        target_sid = redisServ.get_socket_id_by_id(target_id)
+        if target_sid:
+            target = userUtils.get_user(target_id)
+            if target is None:
+                continue
 
-            if target_sid:
-                target = userUtils.get_user(target_id)
-                if target is None:
-                    continue
-
-                error_flag += 1
-                with socket_lock:
-                    socket_io.emit(
-                        "update_distance",
-                        {
-                            "target_id": id,
-                            "distance": userUtils.get_distance(
-                                lat, long, target["latitude"], target["longitude"]
-                            ),
-                        },
-                        room=target_sid,
-                    )
-    except Exception as e:
-        print("update_distance" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+            with socket_lock:
+                socket_io.emit(
+                    "update_distance",
+                    {
+                        "target_id": id,
+                        "distance": userUtils.get_distance(
+                            lat, long, target["latitude"], target["longitude"]
+                        ),
+                    },
+                    room=target_sid,
+                )
 
 
 def unmatch(id, target_id):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
-        if target_id in id_match.get(id, set()):
-            id_match[id].remove(target_id)
+    if target_id in id_match.get(id, set()):
+        id_match[id].remove(target_id)
 
-        error_flag += 1
-        if id in id_match.get(target_id, set()):
-            id_match[target_id].remove(id)
+    if id in id_match.get(target_id, set()):
+        id_match[target_id].remove(id)
 
-        error_flag += 1
-        target_sid = redisServ.get_socket_id_by_id(target_id)
-        if target_sid:
-            with socket_lock:
-                socket_io.emit("unmatch", {"target_id": id}, room=target_sid)
+    user_sid = redisServ.get_socket_id_by_id(id)
+    if user_sid:
+        with socket_lock:
+            socket_io.emit("unmatch", {"target_id": target_id}, room=user_sid)
 
-    except Exception as e:
-        print("unmatch" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    target_sid = redisServ.get_socket_id_by_id(target_id)
+    if target_sid:
+        with socket_lock:
+            socket_io.emit("unmatch", {"target_id": id}, room=target_sid)
 
 
 def unregister(id):
     from wsgi import socket_io, socket_lock
 
-    error_flag = 1
-    try:
-        for target_id in id_match.get(id, set()):
-            target_sid = redisServ.get_socket_id_by_id(target_id)
-            if target_sid:
-
-                error_flag += 1
-                with socket_lock:
-                    socket_io.emit("unregister", {"target_id": id}, room=target_sid)
-
-    except Exception as e:
-        print("unregister" + ("🔥" * error_flag), flush=True)
-        sys.stdout.flush()
+    for target_id in id_match.get(id, set()):
+        target_sid = redisServ.get_socket_id_by_id(target_id)
+        if target_sid:
+            with socket_lock:
+                socket_io.emit("unregister", {"target_id": id}, room=target_sid)
 
 
 #### Utils ####
--- wsgi.py
+++ wsgi.py
@@ -26,28 +26,32 @@ application = socketio.WSGIApp(socket_io, application)
 def handle_connect(sid, environ, auth=None):
     from app.user.userUtils import validation
 
-    if not auth or not auth["id"] or not validation(auth["id"], int):
-        print("auth error: ", auth)
-        sys.stdout.flush()
-        return False
+    with socket_lock:
+        if not auth or not auth["id"] or not validation(auth["id"], int):
+            print("auth error: ", auth)
+            sys.stdout.flush()
+            return False
 
-    socketServ.handle_connect(int(auth["id"]), sid)
+        socketServ.handle_connect(int(auth["id"]), sid)
 
 
 @socket_io.on("disconnect")
 def handle_disconnect(sid):
-    socketServ.handle_disconnect(sid)
+    with socket_lock:
+        socketServ.handle_disconnect(sid)
 
 
 # #### chat ####
 @socket_io.on("send_message")
 def send_message(sid, data):
-    socketServ.send_message(data, sid)
+    with socket_lock:
+        socketServ.send_message(data, sid)
 
 
 @socket_io.on("read_message")
 def read_message(sid, data):
-    socketServ.read_message(data, sid)
+    with socket_lock:
+        socketServ.read_message(data, sid)
 
 
 if __name__ == "__main__":
