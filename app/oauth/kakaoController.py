f0d0ad7dfab82989e571604fd4620a832b095257
juhoh <zestina.oh@gmail.com>
2024-06-18 21:25:43 +0900

    update [Oauth] unite func 2 - login_oauth - register_oauth
    related issue: #6

--- app/oauth/kakaoController.py
+++ app/oauth/kakaoController.py
@@ -67,45 +67,4 @@ class authorize(Resource):
 
         #로그인 후 리 다이렉트
         #TODO [TEST] 리다이렉션 및 데이터 전송
-        return userServ.login_kakao(data['login_id'])
-
-
-# @ns.route('/redirect')
-# class redirect_page(Resource):
-#     def get(self):
-
-#         #TODO error 맞는지 확인 (request.json)
-#         if request.args.get("error"):
-#             raise BadRequest("카카오 로그인 도중 에러가 발생했습니다.")
-
-#         headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
-
-#         data = {'grant_type': 'authorization_code',
-#                 'client_id': client_id,
-#                 'redirect_uri': Kakao.REDIRECT_URI,
-#                 'client_secret': client_secret,
-#                 'code': request.args.get("code")}
-
-#         resp = requests.post(Kakao.TOKEN_URI, data=data, headers=headers)
-#         if resp.status_code != StatusCode.OK:
-#             raise BadRequest("카카오 로그인 도중 에러가 발생했습니다.")
-
-#         headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-#                    'Authorization': f'Bearer {resp.json()['access_token']}'}
-
-#         resp = requests.post(Kakao.DATA_URI, headers=headers)
-#         if resp.status_code != StatusCode.OK:
-#             raise BadRequest("카카오 로그인 도중 에러가 발생했습니다.")
-
-#         kakao_data = resp.json()
-#         data = {'login_id': f'kakao{kakao_data['id']}',
-#                 'email': kakao_data['kakao_account']['email'],
-#                 'name': kakao_data['kakao_account']['profile']['nickname']}
-
-#         #유저 정보 없으면 회원가입
-#         if userUtils.get_user_by_login_id(data['login_id']) is None:
-#             userServ.register_kakao(data)
-
-#         #로그인 후 리 다이렉트
-#         #TODO [TEST] 리다이렉션 및 데이터 전송
-#         return userServ.login_kakao(data['login_id'])
+        return userServ.login_oauth(data['login_id'])
